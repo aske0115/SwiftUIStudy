15d47c727ed0f5eb476368ee66b149b7686b00d3
Aske <burzum17@naver.com>
2021-12-25 18:28:57 +0900

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by GeunHwa Lee on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
