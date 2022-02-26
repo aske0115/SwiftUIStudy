427b46e89ebf9b927ec48b200fb2ba63f7a1b978
Aske <burzum17@naver.com>
2022-02-26 20:46:15 +0900

    textField와 text state와 바인딩을 통하여 값을 출력하고 변경저장하는 부분 칼라피커를 퉁하여 선택된 index를 연동하여 text의 foregroundColor를 변경하는 부분

--- SwiftUIStudy/ContentView.swift
+++ SwiftUIStudy/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    var colors: [Color] = [.black, .red, .green, .blue, .pink]
+    var colorNames = ["Black","Red","Green","Blue","Pink"]
+    
+    @State private var colorIndex = 0
     @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
     
     var body: some View {
         VStack {
@@ -19,7 +25,19 @@ struct ContentView: View {
                     .fontWeight(.heavy)
                     .rotationEffect(.degrees(self.rotation))
                     .animation(.easeInOut(duration: 3), value: rotation)
+                    .foregroundColor(self.colors[colorIndex])
                 Slider(value: $rotation, in: 0...360, step: 0.1)
+                TextField("Enter text here", text:$text)
+                    .textFieldStyle(.roundedBorder)
+                   
+                Picker(selection: $colorIndex, label: Text("Color")) {
+                    ForEach (0..<colorNames.count) {
+                        Text(self.colorNames[$0])
+                            .foregroundColor(self.colors[$0])
+                    }
+                }
+                .foregroundColor(Color("AccentColor"))
+                
             }
         }
     }
