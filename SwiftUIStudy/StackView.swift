1549a4b876727fa159cd073af422284d5019a431
Aske <burzum17@naver.com>
2022-03-20 14:14:11 +0900

    alignmentGuide  ( alignment를 지정해준 subview중에 custom한 offset을 정해줄 수 있는 놈) ( 상위 alignment 값과 alignmentGuide에 alignment 값은 같아야 한다)

--- SwiftUIStudy/StackView.swift
+++ SwiftUIStudy/StackView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct StackView: View {
     var body: some View {
+        VStack (alignment: .leading ){
         HStack(alignment: .firstTextBaseline , spacing:20) {
             Text("Hello, World!1")
             Text("Hello, World!222")
             Text("Hello, World!1412iasufiasugdfiausgdifugasidfuas43")
+        }
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 120, height: 50, alignment: .center)
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 220, height: 50, alignment: .center)
+                .alignmentGuide(.leading) { _ in
+                    return -80
+                }
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 80, height: 50, alignment: .center)
         }
         
     }
