e056f4e7f45ac6b8626f85957a71f62c7575dd6d
Aske <burzum17@naver.com>
2022-03-21 21:43:21 +0900

    algiment-guide-tool

--- SwiftUIStudy/SwiftUIStudyApp.swift
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -14,7 +14,7 @@ struct SwiftUIStudyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView().environmentObject(timerData)
+            AlignmentGuideView().environmentObject(timerData)
         }
     }
 }
